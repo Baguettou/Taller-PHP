0b9291e8ef5a2478149136da4d1cba51795c10dd
juandi247 <jdiaz873@unab.edu.co>
2024-02-27 20:16:20 -0500

    ejercicio 5

---
+++ numeroCubifinitos/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Índice de Ejercicios Parciales</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+
+        .boton {
+            margin: 10px;
+        }
+
+        a {
+            text-decoration: none;
+            color: black;
+
+
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Pagina Principal</h1>
+    <p>Menu de ejercicios</p>
+    <div style="display: flex; justify-content: center; gap: 10px;">
+        <div style="border: 1px solid black; background-color: #d4e4fc; text-align: center; align-items: center;">
+            <a href="home.php" class="boton">Ejercicio 1</a>
+        </div>
+        <div style="border: 1px solid black; background-color: #d4e4fc; text-align: center; align-items: center;">
+            <a href="home.php" class="boton">Ejercicio 2</a>
+        </div>
+        <div style="border: 1px solid black; background-color: #d4e4fc; text-align: center; align-items: center;">
+            <a href="eje3.php" class="boton">Ejercicio 3</a>
+        </div>
+        <div style="border: 1px solid black; background-color: #d4e4fc; text-align: center; align-items: center;">
+            <a href="primos.php" class="boton">Ejercicio 4</a>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
