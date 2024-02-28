e4e1c8cf3569379ede9cb89b4025f90f157d7acd
DanielRHades <drodriguez442@unab.edu.co>
2024-02-27 20:09:41 -0500

    Mi primer cambio

---
+++ php/documento_php.php
@@ -0,0 +1,27 @@
+<pre>
+<?php
+//Aqui se carga toda la base de datos - Vsibles en la URL
+//Usar con formularios con informacion no sensible
+//print_r($_GET);
+//Aqui se carga unicamente lo que solicito - Visibles en la URL
+//print_r($_GET['nombre']);
+//print_r($_POST); 
+//print_r($_SERVER); //Suministra informacion del servidor
+//print_r($_FILES);
+//print_r($_GLOBAL)
+?>
+</pre>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="archivo_php.php" method="get">
+        <h1>Seccion 2: Invitados de una fiesta</h1>
+    </form>
+</body>
+</html>
